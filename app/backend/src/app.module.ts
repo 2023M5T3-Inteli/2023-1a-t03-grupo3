affec3f7911e7ed59c889e28923110125e6a97d2
cffricks <99102201+CFFricks@users.noreply.github.com>
2023-02-23 16:48:03 -0300

    projects-get
    criacao da pagina home(projetos) e chamada get para puxar os projetos

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RankingModule } from './ranking/ranking.module';
+import { LoginModule } from './login/login.module';
+import { HomepageModule } from './homepage/homepage.module';
 
 @Module({
-  imports: [RankingModule],
+  imports: [RankingModule, LoginModule, HomepageModule],
   controllers: [AppController],
   providers: [AppService],
 })
