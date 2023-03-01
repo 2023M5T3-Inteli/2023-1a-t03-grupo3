6fbd09316e72ea9eddb40878e6f285b8e09e9813
naoassisto <99260189+naoassisto@users.noreply.github.com>
2023-03-01 17:25:18 -0300

    Frontend Login (Atualização)

--- app/frontend/src/pages/Login.js
+++ app/frontend/src/pages/Login.js
@@ -25,24 +25,29 @@ export const Login = () => {
                 <div class="flex space-y-1 flex-col mb-24 object-bottom items-center ">
                    
                 <h1 class=" flex flex-col text-white font-bold text-6xl mt-12">Dell Conect</h1>
-                    <p class="flex flex-col text-white" >Log into your account</p>
+                    <p class="flex flex-col text-white my-8" >Log into your account</p>
 
-                    <div class="flex flex-col w-1/2 pr-2 pt-24 object-bottom ">
+                    <div class="flex flex-col w-3/4 pr-2 pt-12 object-bottom ">
                         <label class="text-white">Enter your Dell Email:</label>
                         <input type="text" class="w-full border-2 border-white rounded py-2 px-48 bg-[#ffffff] text-black focus:outline-none focus:border-purple-500" placeholder="email@delltechnologies.com"></input>
                     </div>
                     
-                    <div class="w-1/2 pr-2 pt-8">
+                    <div class="w-3/4 pr-2 pt-8">
                     <label class="text-white"> Enter your password:</label>
                         <input type="email" class=" w-full border-2 border-white rounded py-2 px-48 bg-[#ffffff] text-[#000000] focus:outline-none focus:border-purple-500 mr-8" placeholder="Password"></input>
                     </div>
                     
                     <div class="flex flex-col pt-8 items-center">
-                        <button class="bg-[#8A58DC] text-white font-bold py-2 px-32 rounded ml-10 mr-10">Login</button>
-                        <div class="">
-                            <p class="text-white" > or</p>
-                        </div>                                                                                                                        
-                        <button class="bg-[#f8f8f8] text-[#454545] font-bold py-2 px-16  rounded ml-10 mr-10">Login with Dell SSO</button>
+                        <button class="bg-[#8A58DC] text-white font-bold py-2 w-3/4 rounded ml-10 mr-10">Login</button>
+                        
+                        <div class="flex py-5 items-center">
+                            <div class="flex-grow border-t border-gray-400 h-0.5"></div>
+                                <span class="flex-shrink mx-4 text-gray-400">Or</span>
+                            <div class="flex-grow border-t border-gray-400 h-0.5"></div>
+                        </div> 
+
+                        <button class="bg-[#f8f8f8] text-[#454545] font-bold py-2 w-3/4 rounded ml-10 mr-10">Login with Dell SSO</button>
+                    
                     </div>
                 
                 </div>
