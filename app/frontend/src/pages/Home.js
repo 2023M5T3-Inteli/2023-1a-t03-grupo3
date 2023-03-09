1f602440186eb4c7f09c47ca5a017f00933f56ea
marcelofeitoza <feitoza.marcelo7@gmail.com>
2023-03-09 15:32:04 -0300

    Merge branch 'main' of https://github.com/2023M5T3-Inteli/2023-1a-t03-grupo3 into frontend-home
    Co-authored-by: Sofia Pimazzoni <sofipimazzoni@users.noreply.github.com>

--- app/frontend/src/pages/Home.js
+++ app/frontend/src/pages/Home.js
@@ -1,5 +1,8 @@
 import { Sidebar } from '../components/Sidebar'
 import { Card } from '../components/Card'
+import filter from '../assets/filter.svg'
+import search from '../assets/search.svg'
+
 
 export const Home = () => {
     const cards = [
@@ -24,21 +27,34 @@ export const Home = () => {
         <div className="bg-[#E1E1E1] flex min-h-screen max-w-screen w-full h-full">
             <Sidebar />
 
-            <div className=" w-4/5 flex flex-col space-y-4 ml-8 mr-8 mt-4"  >
-            <div>
-             <input class="w-full border-2 border-gray-300 bg-white h-10 px-5 pr-10 rounded-md text-sm focus:outline-none" type="search" name="search" placeholder="Pesquisar"/>
-            </div>
+            <div className="bg-[#E1E1E1] w-4/5 flex flex-col space-y-4 ml-8 mr-8 mt-4"  >
+                    <div class="relative">
+                        <input type="text" placeholder="Search for Projects" class="py-2 pl-5 pr- border border-gray-400 rounded-full w-full" />
+                        <div class="absolute inset-y-0 right-0 flex items-center ">
+                            <button onclick="openModal()"><img src={filter} className="mr-2"></img></button>
+
+                            <button type="button" class="px-4 py-2 bg-purple-500 hover:bg-purple-600 text-white font-bold rounded-r-full "> <img src={search}></img>
+                            </button>
+
+                        </div>
+
+                    </div>
 
 
-            <p className='text-[#4A92FF] mb-2 pt-6 font-bold text-lg'>Trending Projects</p>
-                {cards.map(item => {
-                    return (
-                        <Card title={item.title} desc={item.desc} 
-                        />
-                        
-                    )
-                })}
-            </div>
+
+
+
+                    <p className='text-[#4A92FF] mb-2 pt-6 font-bold text-lg'>Trending Projects</p>
+
+                    {cards.map(item => {
+                        return (
+                            <Card title={item.title} desc={item.desc}
+                            />
+
+                        )
+                    })}
+                </div>
+           
         </div>
     )
 }
