0ac4025dc0f4531f6d56422868d8d85ec888162b
naoassisto <99260189+naoassisto@users.noreply.github.com>
2023-03-15 17:44:49 -0300

    Concertos Endpoint token auth

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AuthModuleOptions } from '@nestjs/passport';
+import { AuthModule } from './auth/auth.module';
 import { HomepageModule } from './homepage/homepage.module';
 import { LoginModule } from './login/login.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { ProfileModule } from './profile/profile.module';
+import { requestModule } from './projectRequest/request.module';
 import { RankingModule } from './ranking/ranking.module';
 
 @Module({
-  imports: [RankingModule, NotificationsModule, LoginModule, HomepageModule, ProfileModule]
+  imports: [RankingModule, NotificationsModule, LoginModule, HomepageModule, ProfileModule, AuthModule, requestModule]
 })
 export class AppModule {}
---
+++ app/backend/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
+// import { HttpException, HttpStatus } from '@nestjs/common';
+
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'yourSecretKey',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+})
+export class AuthModule {}
+
