692c017989e3a4a439445e7bfdfb989e11f19222
Ubuntu <ubuntu@ip-10-0-1-65.ec2.internal>
2023-03-20 18:47:50 +0000

    Testing actions

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,4 +17,7 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
+
+
 bootstrap();
