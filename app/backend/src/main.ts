a171ede881ce99cbdd68f7f5156f659bd7f88973
Marcelo G Feitoza <71825192+marcelofeitoza@users.noreply.github.com>
2023-03-20 15:58:58 -0300

    test action

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -15,5 +17,6 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
 
 bootstrap();
