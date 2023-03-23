2b913a10f855a9c52b8947e2a118e0159d00cb60
marcelofeitoza <feitoza.marcelo7@gmail.com>
2023-03-23 04:21:48 -0300

    routes fixing - integrations

---
+++ app/backend/src/projects/dto/createProject.dto.ts
@@ -0,0 +1,32 @@
+
+import { IsArray, IsDate, IsDateString, IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+
+export class CreateProjectDto {
+    @IsNotEmpty()
+    @IsString()
+    public title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    public description: string;
+
+    @IsNotEmpty()
+    @IsString()
+    public startDate: string;
+
+    @IsNotEmpty()
+    @IsString()
+    public endDate: string;
+
+    @IsNotEmpty()
+    @IsString()
+    public category: string;
+
+    @IsArray()
+    @IsNotEmpty()
+    public questions: string[];
+
+    @IsNotEmpty()
+    @IsArray()
+    public tags: string[];
+}
---
+++ app/backend/src/projects/dto/updateProject.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateProjectDto } from './createProject.dto';
+
+export class UpdateProjectDto extends PartialType(CreateProjectDto) { }
